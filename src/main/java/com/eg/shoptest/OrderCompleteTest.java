b1d5daa8c955057d153fa118423a3a16bfdd8eb5
Eli Guy <eliguy343@gmail.com>
2022-11-10 01:50:42 +0200

    added quantity select & proceed to checkout to test

--- src/main/java/com/eg/shoptest/OrderCompleteTest.java
+++ src/main/java/com/eg/shoptest/OrderCompleteTest.java
@@ -2,13 +2,17 @@ package com.eg.shoptest;
 
 import java.io.IOException;
 
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import base.BasePage;
 import pageObjects.Homepage;
+import pageObjects.ShopContentPanel;
 import pageObjects.ShopHomepage;
+import pageObjects.ShopProductPage;
+import pageObjects.ShoppingCart;
 
 public class OrderCompleteTest extends BasePage {
 
@@ -31,13 +35,38 @@ public class OrderCompleteTest extends BasePage {
 	@Test
 	public void endToEndTest() throws InterruptedException {
 		Homepage home = new Homepage(driver);
+		
 		Thread.sleep(2000);
 		home.getToggle().click();
+		
 		Thread.sleep(2000);
 		home.getTestStoreLink().click();
+		
 		Thread.sleep(2000);
 		ShopHomepage  shopHome = new ShopHomepage(driver);
 		shopHome.getProdOne().click();
+		
+		Thread.sleep(2000);
+		ShopProductPage shopProd = new ShopProductPage(driver);
+		Select option = new Select(shopProd.getSizeDropDownMenu());
+		option.selectByVisibleText("M");
+		shopProd.getQuantityIncrease().click();
+		shopProd.getAddToCartBtn().click();
+		
+		Thread.sleep(2000);
+		ShopContentPanel cPanel = new ShopContentPanel(driver);
+		cPanel.getProceedToCheckoutBtn().click();
+		
+		ShoppingCart cart = new ShoppingCart(driver);
+		cart.getHavePromo().click();
+		cart.getPromoTextBox().sendKeys("20OFF");
+		cart.getPromoAddBtn().click();
+		
+		Thread.sleep(3000);
+		cart.getProceedCheckoutBtn();
+		
+		
+		
 	}
 
 }
